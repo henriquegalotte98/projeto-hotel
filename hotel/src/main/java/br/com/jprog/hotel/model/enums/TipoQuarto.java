ebd32123204b7df0d3c888c17a4577b4d24d42fd
Alexandre Alves <ditecalexandre@gmail.com>
2026-08-10 21:06:05 -0300

    feat: adiciona enums de quartos documentados

---
+++ hotel/src/main/java/br/com/jprog/hotel/model/enums/TipoQuarto.java
@@ -0,0 +1,16 @@
+package br.com.jprog.hotel.model.enums;
+
+/**
+ * Define as categorias de quarto oferecidas pelo hotel.
+ */
+public enum TipoQuarto {
+
+    /** Quarto da categoria básica. */
+    SIMPLES,
+
+    /** Quarto preparado para acomodação dupla. */
+    DUPLO,
+
+    /** Quarto da categoria suíte. */
+    SUITE
+}
